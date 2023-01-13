fb298eb11cbf9281a6c4faa101dcb4f01c8f45d8
GS <76037288+ANXIAOFEN@users.noreply.github.com>
2023-01-13 21:16:03 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -4,8 +4,7 @@ from torch.utils.data import Dataset
 
 
 class MyDataSet(Dataset):
-    """自定义数据集"""
-
+   
     def __init__(self, images_path: list, images_class: list, transform=None):
         self.images_path = images_path
         self.images_class = images_class
@@ -16,7 +15,7 @@ class MyDataSet(Dataset):
 
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
-        # RGB为彩色图片，L为灰度图片
+       
         if img.mode != 'L':
             raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
         label = self.images_class[item]
@@ -28,8 +27,7 @@ class MyDataSet(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        # 官方实现的default_collate可以参考
-        # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
+      
         images, labels = tuple(zip(*batch))
 
         images = torch.stack(images, dim=0)
